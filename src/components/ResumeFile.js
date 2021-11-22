2619052418c38252e634e66283c2da9e0d645e50
abdullah-mohammed <aimohammed@ucdavis.edu>
2021-11-21 21:14:35 -0800

    finished ResumeFile and ResumeModal

--- src/components/ResumeFile.js
+++ src/components/ResumeFile.js
@@ -8,15 +8,17 @@ import { Tooltip, Button } from 'react95';
 
 const ResumeFile = () => {
     const [open, setOpen] = useState(false); 
+    const resumeLink = 'https://drive.google.com/file/d/1w4LVAuOm9ZIdl1zILhfP3RKk1x5mkVOH/view?usp=sharing'; 
 
     return (
         <div>
             <Tooltip text='Double Click Me!' enterDelay={100} leaveDelay={300}>
                 <img
-                    src={resumeClickable}
+                    src = {resumeClickable}
                     alt = 'resume clickable'
-                    style={{ height: 150}}
+                    style = {{ height: 150}}
                     onDoubleClick = {() => { setOpen(true) }}
+                    onTouchStart = {() => { window.open(resumeLink) }}
                 />
                 <h1 style = {{textAlign: 'center'}}>Resume.pdf</h1>
             </Tooltip>
--- src/components/ResumeModal.js
+++ src/components/ResumeModal.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 //styles related 
 import {
     Window,
@@ -5,15 +6,35 @@ import {
     WindowContent,
     Cutout,
     Button, 
-    Toolbar
+    Toolbar,
+    LoadingIndicator,
+    Panel
 } from 'react95';
 import {ModalWrapper} from './ResumeModal.styles'; 
+//components 
+//import { Document, Page } from 'react-pdf';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+//files
+import pdfFile from '../downloadFiles/AbdullahMResume.pdf'; 
+
 
 const ResumeModal = (props) => {
+    const [scale, setScale] = useState(1.5); 
+    const [loading, setLoading] = useState(false); 
+
+    const incScale = () => {
+        let currScale = scale; 
+        setScale(currScale + 0.1); 
+    }
+    const decScale = () => {
+        let currScale = scale; 
+        setScale(currScale - 0.1); 
+    }
+
     return (
         <ModalWrapper>
-            <Window style = {{marginLeft: 'auto', marginRight: 'auto', marginTop: '25vw', height: 'max-content'}}>
-                <WindowHeader>
+            <Window style = {{marginLeft: 'auto', marginRight: 'auto', marginTop: '10vh', height: 'max-content'}}>
+                <WindowHeader style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                     <span>Resume.pdf</span>
                     <Button onClick = {() => { props.modifyOpen(false)}}>
                         <span role='img' aria-label='close'>
@@ -21,9 +42,39 @@ const ResumeModal = (props) => {
                         </span>
                     </Button>
                 </WindowHeader>
-                <WindowContent>
-
+                <WindowContent style = {{height: 'max-content'}}>
+                    <div style = {{overflowY: 'auto', maxHeight: '70vh', maxWidth: '90vw'}}>
+                        <Document
+                            file = {pdfFile}
+                            loading = {() => {
+                                setLoading(true); 
+                                return (
+                                    <div style = {{width: '25vw'}}>
+                                        <p style={{ textAlign: 'center', marginBottom: '0.5rem' }}>Loading...</p>
+                                        <LoadingIndicator isLoading />
+                                    </div>
+                                );
+                            }}
+                            onLoadSuccess = {() => { setLoading(false) }}
+                        >
+                            <Page pageNumber = {1} scale = {scale}/>
+                        </Document>
+                    </div>
                 </WindowContent>
+                {!loading && 
+                    <div style = {{display: 'flex', justifyContent: 'end', paddingRight: '15px'}}>
+                        <Button onClick = {() => {decScale()}} style = {{marginRight: '5px'}}>
+                            <span role='img' aria-label='decrease'>
+                                ➖
+                            </span>
+                        </Button>
+                        <Button onClick = {() => {incScale()}}>
+                            <span role='img' aria-label='increase'>
+                                ➕
+                            </span>
+                        </Button>
+                    </div>
+                }
             </Window>
         </ModalWrapper>
     ); 
